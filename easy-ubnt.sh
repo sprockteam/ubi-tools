7dd7f84571c17f12fe9a6fce842b3eeb315dd023
Klint Van Tassel <developer@sprocktech.com>
2019-05-21 02:50:55 -0500

    Added option to specifiy SSH port from command line

--- easy-ubnt.sh
+++ easy-ubnt.sh
@@ -2031,7 +2031,7 @@ function __eubnt_install_dependencies()
 # Hardening the OpenSSH Server config according to best practices (https://gist.github.com/nvnmo/91a20f9e72dffb9922a01d499628040f | https://linux-audit.com/audit-and-harden-your-ssh-configuration/)
 # De-duplicate SSH config file (https://stackoverflow.com/a/1444448)
 function __eubnt_setup_ssh_server() {
-  if [[ -n "${__ssh_skip:-}" ]]; then
+  if [[ -n "${__option_sshd_setup:-}" && "${__option_sshd_setup}" = "skip" ]]; then
     return 1
   fi
   __eubnt_show_header "Setting up OpenSSH Server..."
@@ -2049,17 +2049,26 @@ function __eubnt_setup_ssh_server() {
     __eubnt_show_notice "Checking OpenSSH server settings for recommended changes..."
     echo
     if [[ $(grep ".*Port 22$" "${__sshd_config}") || ! $(grep ".*Port.*" "${__sshd_config}") ]]; then
-      if __eubnt_question_prompt "Change SSH port from the default 22?" "return" "n"; then
-        local ssh_port=""
-        while [[ ! "${ssh_port:-}" =~ ^([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$ ]]; do
-          __eubnt_get_user_input "Port number to use: " "ssh_port"
-        done
-        if grep --quiet ".*Port.*" "${__sshd_config}"; then
-          sed -i "s/^.*Port.*$/Port ${ssh_port}/" "${__sshd_config}"
-        else
-          echo "Port ${ssh_port}" | tee -a "${__sshd_config}"
+      __sshd_port=$(grep "Port" "${__sshd_config}" --max-count=1 | awk '{print $NF}')
+      local sshd_port=""
+      if [[ -n "${__option_sshd_setup:-}" && "${__option_sshd_setup}" =~ ${__regex_port_number} ]]; then
+        if [[ "${__option_sshd_setup}" != "${__sshd_port:-22}" ]]; then
+          sshd_port="${__option_sshd_setup}"
         fi
-        __sshd_port="${ssh_port}"
+      else
+        if __eubnt_question_prompt "Change SSH port from the default 22?" "return" "n"; then
+          while [[ ! "${sshd_port:-}" =~ ^([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$ ]]; do
+            __eubnt_get_user_input "Port number to use: " "sshd_port"
+          done
+        fi
+      fi
+      if [[ -n "${sshd_port:-}" && "${sshd_port}" =~ ${__regex_port_number} ]]; then
+        if grep --quiet ".*Port.*" "${__sshd_config}"; then
+          sed -i "s/^.*Port.*$/Port ${sshd_port}/" "${__sshd_config}"
+        else
+          echo "Port ${sshd_port}" | tee -a "${__sshd_config}"
+        fi
+        __sshd_port="${sshd_port}"
         __restart_ssh_server=true
       fi
     fi
